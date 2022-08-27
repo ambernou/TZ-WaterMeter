15ad7a7cdc3fd840c77a3ec33227581e5ffede43
NatalyaSemenova <ambernou@gmail.com>
2022-08-27 12:59:30 +0700

    init vue

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import serveStatic from 'serve-static'
+import path from 'path'
+
+const app = express()
+
+app.use('/', serveStatic(path.join(__dirname, '/vue-water-meter', '/dist')))
+
+const port = process.env.PORT || 3000
+
+app.listen(port)
