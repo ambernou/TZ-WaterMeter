986eb480b293f2b110185d9fa88e47f139ee72e8
NatalyaSemenova <ambernou@gmail.com>
2022-08-27 15:46:11 +0700

    vue input

---
+++ vue-water-meter/src/components/NumberInput.vue
@@ -0,0 +1,37 @@
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'NumberInputComp',
+  data(){
+    return{
+      form: {
+        number: ''
+      }
+    }
+  },
+  methods: {
+    form_handler(){
+      axios.post('/number', this.form)
+      .then((res) => console.log('ответ'))
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h2>Введите номер лицевого счета:</h2>
+    <form @submit.prevent="form_handler">
+      <div>
+        <label for="number"></label>
+        <input type="text" id="number" placeholder="Ваш номер" v-model="form.number">
+      </div>
+      <button>Отправить</button>
+    </form>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
